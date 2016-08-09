abb8f493f8292dc3b16d7c1ccc433f790e4049e6
whatever823301233 <shun7ziran>
2016-08-09 23:34:26 +0800

    添加附近列表和地图fragment，未实现页面以及功能代码

--- app/src/main/java/com/qiang/qiangguide/activity/MainGuideActivity.java
+++ app/src/main/java/com/qiang/qiangguide/activity/MainGuideActivity.java
@@ -1,8 +1,12 @@
 package com.qiang.qiangguide.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.qiang.qiangguide.R;
+import com.qiang.qiangguide.fragment.MapFragment;
+import com.qiang.qiangguide.fragment.NearExhibitFragment;
 
 public class MainGuideActivity extends ActivityBase {
 
@@ -11,12 +15,17 @@ public class MainGuideActivity extends ActivityBase {
 
     /**
      * Optionally used with {@link #EXTRA_START_FULLSCREEN} to carry a MediaDescription to
-     * the {@link FullScreenPlayerActivity}, speeding up the screen rendering
+     * the {@link PlayActivity}, speeding up the screen rendering
      * while the {@link android.support.v4.media.session.MediaControllerCompat} is connecting.
      */
     public static final String EXTRA_CURRENT_MEDIA_DESCRIPTION =
             "com.example.android.uamp.CURRENT_MEDIA_DESCRIPTION";
+    private NearExhibitFragment exhibitListFragment;
+    private MapFragment mapFragment;
 
+
+    public static final String INTENT_FLAG_GUIDE_MAP="intent_flag_guide_map";
+    public static final String INTENT_FLAG_GUIDE="intent_flag_guide";
 
 
     @Override
@@ -24,6 +33,26 @@ public class MainGuideActivity extends ActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_guide);
     }
+
+    /**
+     * 设置默认fragment
+     */
+    private void setDefaultFragment() {
+        String flag=getIntent().getStringExtra(INTENT_FLAG_GUIDE_MAP);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        exhibitListFragment = NearExhibitFragment.newInstance();
+        mapFragment = MapFragment.newInstance();
+        if (flag.equals(INTENT_FLAG_GUIDE)){
+            transaction.replace(R.id.llExhibitListContent, exhibitListFragment);
+            //radioButtonList.setChecked(true);
+        }else{
+            transaction.replace(R.id.llExhibitListContent, mapFragment);
+            //radioButtonMap.setChecked(true);
+        }
+        transaction.commit();
+    }
+
 
     @Override
     void errorRefresh() {
---
+++ app/src/main/java/com/qiang/qiangguide/fragment/NearExhibitFragment.java
@@ -0,0 +1,89 @@
+package com.qiang.qiangguide.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qiang.qiangguide.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link NearExhibitFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link NearExhibitFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NearExhibitFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public NearExhibitFragment() {
+    }
+
+    public static NearExhibitFragment newInstance() {
+        NearExhibitFragment fragment = new NearExhibitFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }*/
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_near_exhibit, container, false);
+    }
+
+    /*
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }*/
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        /*if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        //mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        
+        void onFragmentInteraction(Uri uri);
+    }
+}
