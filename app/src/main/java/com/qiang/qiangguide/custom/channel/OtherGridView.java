76db9b8d1e8fd11b929531b0ad262a87caf3a814
whatever823301233 <shun7ziran>
2016-08-11 23:47:16 +0800

    增加专题标签选择页

---
+++ app/src/main/java/com/qiang/qiangguide/custom/channel/OtherGridView.java
@@ -0,0 +1,22 @@
+package com.qiang.qiangguide.custom.channel;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by xq823 on 2016/8/11.
+ */
+public class OtherGridView extends GridView {
+
+    public OtherGridView(Context paramContext, AttributeSet paramAttributeSet) {
+        super(paramContext, paramAttributeSet);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
